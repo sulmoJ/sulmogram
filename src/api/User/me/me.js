22e797ff970b87f8e4a80f559cb3a1c8b75911c9
SungMo <tjdah0853@gmail.com>
2020-08-11 21:55:41 +0900

    20200811

--- src/api/User/me/me.js
+++ src/api/User/me/me.js
@@ -2,24 +2,15 @@ import { prisma } from "../../../../generated/prisma-client";
 export default {
   Query: {
     // __(double under score는 부모 arguments를 뜻함)
-    me: (_, __, { request, isAuthenticated }) => {
+    me: async (_, __, { request, isAuthenticated }) => {
       isAuthenticated(request);
       const { user } = request;
-      
-      return prisma.user({ id: user.id }).$fragment(`
-      fragment UserParts on User{
-        id
-        username
-        email
-        firstName
-        lastName
-        bio
-        posts{
-          id
-          caption
-        }
-      }
-    `);
+      const userProfile = await prisma.user({ id: user.id });
+      const posts = await prisma.user({ id: user.id }).posts();
+      return {
+        user: userProfile,
+        posts,
+      };
     },
   },
 };
