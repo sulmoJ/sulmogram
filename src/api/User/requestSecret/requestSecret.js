7034886a7313edcdef955091a61e849136542206
SungMo <tjdah0853@gmail.com>
2020-08-11 15:20:42 +0900

    searchUser and searchPost resolver

--- src/api/User/requestSecret/requestSecret.js
+++ src/api/User/requestSecret/requestSecret.js
@@ -3,12 +3,10 @@ import { prisma } from "../../../../generated/prisma-client";
 
 export default {
   Mutation: {
-    requestSecret: async (_, args, { request }) => {
-      console.log(request.user);
+    requestSecret: async (_, args) => {
       const { email } = args;
       const loginSecret = generateSecret();
       try {
-        throw Error();
         await sendSecretMail(email, loginSecret);
         await prisma.updateUser({ data: { loginSecret }, where: { email } });
         return true;
